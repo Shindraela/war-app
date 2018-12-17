443ff7d5f247d6115619bb8ec9f5bfd515555956
dylanolv <oliveira.dylann@gmail.com>
2018-12-17 20:42:31 +0100

    Récupération du deck_id

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Cards extends Component {
     constructor() {
         super();
         this.state = {
             cards: []
-        }
-    };
+        };
+    }
 
     componentDidMount() {
         this.fetchData();
     }
 
     fetchData = () => {
-        fetch("https://deckofcardsapi.com/api/deck/kknckzvf8757/draw/?count=2")
+        fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
             .then(results => {
-                return results.json()
-            }).then(data => {
-                // console.log("data :", data);
-                let cards = data.cards.map((card) => {
+                return results.json();
+            })
+            .then(data => {
+                const deck_id = data.deck_id;
+                console.log(deck_id);
+                let cards = data.cards.map(card => {
                     return (
                         <div key={card.code}>
                             <img src={card.images.png} alt="card" />
                         </div>
-                    )
-                })
+                    );
+                });
                 this.setState({ cards: cards });
-            })
-    }
+            });
+    };
 
     render() {
         return (
             <div>
                 {this.state.cards}
-                <button onClick={this.fetchData}>Show 2 more cards</button>
+                <button onClick={this.fetchData}>Show 2 more card</button>
             </div>
-        )
+        );
     }
 }
 
