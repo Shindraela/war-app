90cd1d5ad480fc560897843a833f5c953cfd68d5
Farooq Asmaa <farooq.asmaa@gmail.com>
2018-12-17 21:34:05 +0100

    Split deck

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,13 +13,34 @@ class Cards extends Component {
     }
 
     fetchData = () => {
-        fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
+        fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=52")
             .then(results => {
                 return results.json();
             })
             .then(data => {
                 const deck_id = data.deck_id;
-                console.log(deck_id);
+                // console.log(deck_id);
+
+                // Split deck into two piles for player and bot
+                let playerDeck = data.cards.slice(0, 26);
+                let botDeck = data.cards.slice(26, 52);
+
+                // Make string with player cards code for fetch API
+                let playerCardsArray = [];
+                playerDeck.map(card => {
+                    playerCardsArray.push(card.code);
+                });
+                let playersCardsString = playerCardsArray.join(",");
+                console.log("playersCardsString :", playersCardsString);
+
+                // Same for bot cards
+                let botCardsArray = [];
+                botDeck.map(card => {
+                    botCardsArray.push(card.code);
+                });
+                let botsCardsString = botCardsArray.join(",");
+                console.log("botsCardsString :", botsCardsString);
+
                 let cards = data.cards.map(card => {
                     return (
                         <div key={card.code}>
